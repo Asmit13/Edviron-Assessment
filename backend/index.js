46cda3913d13fe189718789aeac0aa21bf04a489
Asmit Aditya Singh <asmitadsingh13@gamil.com>
2025-09-22 13:31:45 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,74 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/auth");
+const paymentRoutes = require("./routes/payments");
+const webhookRoutes = require("./routes/webhook");
+const transactionsRoutes = require("./routes/transactions");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output.json");
+const {
+  SwaggerUIBundle,
+  SwaggerUIStandalonePreset,
+} = require("swagger-ui-dist");
+
+const app = express();
+// app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://edviron-gateway.vercel.app",
+  "https://edviron-gateway-backend.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed for this origin"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) =>
+  res.json({
+    ok: true,
+    status: "Backend is Up and Running",
+    message:
+      "Backend for School Payments Dashboard gateway - Programmed By : patelcodes@gmail.com",
+  })
+);
+
+app.use("/auth", authRoutes);
+app.use("/payments", paymentRoutes);
+app.use("/webhook", webhookRoutes);
+app.use("/transactions", transactionsRoutes);
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerFile, {
+    customCssUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.29.0/swagger-ui.min.css",
+  })
+);
+
+// error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  const status = err.status || 500;
+  res.status(status).json({ error: err.message || "Internal Server Error" });
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server running on port ${port}`));
